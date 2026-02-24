2fdfa211b225d19d6345557a4c533bd02689c4e5
lankalapalli kumar <kumar7674098939@gmail.com>
2026-02-24 22:14:00 +0530

    debug groq env

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from core_rag import build_rag_chain
 from config import config
 from langchain_core.messages import HumanMessage, AIMessage
+import os
+import streamlit as st
+
+st.write("HF GROQ KEY PRESENT:", bool(os.getenv("GROQ_API_KEY")))
 
 load_dotenv()
 
